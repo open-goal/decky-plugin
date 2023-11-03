d598ec2239bdc72b817f6dffba4eaa86d2403473
Tyler Wilding <xTVaser@users.noreply.github.com>
2023-11-03 00:04:06 -0400

    Add support for Jak 2 (#4)
    * jak2: add jak2 assets for shortcut
    
    * jak2: add support for installing and running jak2
    
    * ci: setup a CI workflow

--- src/index.tsx
+++ src/index.tsx
@@ -58,6 +58,12 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
   const [jak1OutOfDate, setJak1OutOfDate] = useState(false);
   const [jak1ShortcutAlreadyExists, setJak1ShortcutAlreadyExists] =
     useState(false);
+  const [jak2Installed, setJak2Installed] = useState(false);
+  const [jak2ISOExists, setJak2ISOExists] = useState(false);
+  const [jak2Installing, setJak2Installing] = useState(false);
+  const [jak2OutOfDate, setJak2OutOfDate] = useState(false);
+  const [jak2ShortcutAlreadyExists, setJak2ShortcutAlreadyExists] =
+    useState(false);
 
   useEffect(() => {
     async function loadDefaults() {
@@ -67,35 +73,67 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
       if (typeof homeDir === "string") {
         setUsersHomeDir(homeDir);
       }
-      const isInstalled = (
+      // Jak 1
+      const isJak1Installed = (
         await serverAPI.callPluginMethod("is_game_installed", { game: "jak1" })
       ).result;
-      window.console.log(`Jak 1 Installed?: ${isInstalled}`);
-      setJak1Installed(!!isInstalled);
-      const isoExists = (
+      window.console.log(`Jak 1 Installed?: ${isJak1Installed}`);
+      setJak1Installed(!!isJak1Installed);
+      const jak1IsoExists = (
         await serverAPI.callPluginMethod("does_iso_exist_for_installation", {
           game: "jak1",
         })
       ).result;
-      window.console.log(`Jak 1 ISO Exists?: ${isoExists}`);
-      setJak1ISOExists(!!isoExists);
-      const outOfDate = (
+      window.console.log(`Jak 1 ISO Exists?: ${jak1IsoExists}`);
+      setJak1ISOExists(!!jak1IsoExists);
+      const jak1OutOfDate = (
         await serverAPI.callPluginMethod("is_game_out_of_date", {
           game: "jak1",
         })
       ).result;
-      window.console.log(`Jak 1 outOfDate?: ${outOfDate}`);
-      setJak1OutOfDate(!!outOfDate);
-      const shortcutAlreadyExists = (
+      window.console.log(`Jak 1 outOfDate?: ${jak1OutOfDate}`);
+      setJak1OutOfDate(!!jak1OutOfDate);
+      const jak1ShortcutAlreadyExists = (
         await serverAPI.callPluginMethod("shortcut_already_created", {
           owner_id: getCurrentSteamUserId(),
           game: "jak1",
         })
       ).result;
       window.console.log(
-        `Jak 1 shortcutAlreadyExists?: ${shortcutAlreadyExists}`
+        `Jak 1 shortcutAlreadyExists?: ${jak1ShortcutAlreadyExists}`
       );
-      setJak1ShortcutAlreadyExists(!!shortcutAlreadyExists);
+      setJak1ShortcutAlreadyExists(!!jak1ShortcutAlreadyExists);
+
+      // Jak 2
+      const isJak2Installed = (
+        await serverAPI.callPluginMethod("is_game_installed", { game: "jak2" })
+      ).result;
+      window.console.log(`Jak 2 Installed?: ${isJak2Installed}`);
+      setJak2Installed(!!isJak2Installed);
+      const jak2IsoExists = (
+        await serverAPI.callPluginMethod("does_iso_exist_for_installation", {
+          game: "jak2",
+        })
+      ).result;
+      window.console.log(`Jak 2 ISO Exists?: ${jak2IsoExists}`);
+      setJak2ISOExists(!!jak2IsoExists);
+      const jak2OutOfDate = (
+        await serverAPI.callPluginMethod("is_game_out_of_date", {
+          game: "jak2",
+        })
+      ).result;
+      window.console.log(`Jak 2 outOfDate?: ${jak2OutOfDate}`);
+      setJak2OutOfDate(!!jak2OutOfDate);
+      const jak2ShortcutAlreadyExists = (
+        await serverAPI.callPluginMethod("shortcut_already_created", {
+          owner_id: getCurrentSteamUserId(),
+          game: "jak2",
+        })
+      ).result;
+      window.console.log(
+        `Jak 2 shortcutAlreadyExists?: ${jak2ShortcutAlreadyExists}`
+      );
+      setJak2ShortcutAlreadyExists(!!jak2ShortcutAlreadyExists);
       setComponentLoaded(true);
     }
     loadDefaults();
@@ -107,9 +145,7 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
         strTitle={"Delete Jak 1?"}
         strCancelButtonText={"Cancel"}
         strOKButtonText={"Delete"}
-        strDescription={
-          "Are you sure you want to delete Jak 1?"
-        }
+        strDescription={"Are you sure you want to delete Jak 1?"}
         onOK={async () => {
           const success = (
             await serverAPI.callPluginMethod("remove_game", {
@@ -118,7 +154,8 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
           ).result;
           if (success) {
             setJak1Installed(false);
-          }}}
+          }
+        }}
       />
     );
   };
@@ -148,6 +185,31 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
     }
   };
 
+  const jak2InstallationHelperText = () => {
+    if (jak2Installed) {
+      if (jak2OutOfDate) {
+        return `${usersHomeDir}/OpenGOAL/games/jak2 is out of date!`;
+      } else {
+        return `Already installed in: ${usersHomeDir}/OpenGOAL/games/jak2`;
+      }
+    } else if (!jak2ISOExists) {
+      return `${usersHomeDir}/OpenGOAL/isos/jak2.iso not found, can't install!`;
+    } else {
+      return `Installs Jak 2 using ${usersHomeDir}/OpenGOAL/isos/jak2.iso`;
+    }
+  };
+
+  const jak2InstallButtonContent = () => {
+    if (jak2Installing) {
+      return <Spinner width={16} height={16}></Spinner>;
+    } else {
+      if (jak2OutOfDate) {
+        return "Update Jak 2";
+      }
+      return "Install Jak 2";
+    }
+  };
+
   const loadedComponent = () => {
     return (
       <>
@@ -166,7 +228,11 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
             >
               <Focusable style={{ display: "flex" }}>
                 <DialogButton
-                  disabled={jak1Installing || !jak1ISOExists || (jak1Installed && !jak1OutOfDate)}
+                  disabled={
+                    jak1Installing ||
+                    !jak1ISOExists ||
+                    (jak1Installed && !jak1OutOfDate)
+                  }
                   onClick={async () => {
                     if (jak1OutOfDate) {
                       setJak1Installing(true);
@@ -227,19 +293,85 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
               </Focusable>
             </Field>
           </PanelSectionRow>
-          {/* <PanelSectionRow>
-        <Field childrenLayout="below" description="Installs Jak 2 using your `jak2.iso` in `/home/deck/OpenGOAL/isos`">
-          <DialogButton
-            onClick={async () => {
-              await serverAPI.callPluginMethod("install_game", {
-                game: "jak2"
-              });
-            }}
-          >
-            Install Jak 2
-          </DialogButton>
-        </Field>
-      </PanelSectionRow> */}
+          <PanelSectionRow>
+            <Field
+              bottomSeparator="none"
+              icon={null}
+              label={null}
+              childrenLayout={undefined}
+              inlineWrap="keep-inline"
+              padding="none"
+              spacingBetweenLabelAndChild="none"
+              childrenContainerWidth="max"
+              description={jak2InstallationHelperText()}
+            >
+              <Focusable style={{ display: "flex" }}>
+                <DialogButton
+                  disabled={
+                    jak2Installing ||
+                    !jak2ISOExists ||
+                    (jak2Installed && !jak2OutOfDate)
+                  }
+                  onClick={async () => {
+                    if (jak2OutOfDate) {
+                      setJak2Installing(true);
+                      const success = (
+                        await serverAPI.callPluginMethod("update_game", {
+                          game: "jak2",
+                        })
+                      ).result;
+                      setJak2Installing(false);
+                      if (
+                        success !== null &&
+                        success !== undefined &&
+                        success
+                      ) {
+                        setJak2Installed(true);
+                        setJak2OutOfDate(false);
+                      }
+                    } else {
+                      setJak2Installing(true);
+                      const success = (
+                        await serverAPI.callPluginMethod("install_game", {
+                          game: "jak2",
+                        })
+                      ).result;
+                      setJak2Installing(false);
+                      if (
+                        success !== null &&
+                        success !== undefined &&
+                        success
+                      ) {
+                        setJak2Installed(true);
+                        setJak2OutOfDate(false);
+                      }
+                    }
+                  }}
+                >
+                  {jak2InstallButtonContent()}
+                </DialogButton>
+                {jak2Installed ? (
+                  <DialogButton
+                    disabled={jak2Installing}
+                    onClick={async () => {
+                      showConfirmDeletionModal();
+                    }}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: "10px",
+                      maxWidth: "40px",
+                      minWidth: "auto",
+                      marginLeft: ".5em",
+                    }}
+                  >
+                    <MdDeleteForever fill="#ff0000" />
+                  </DialogButton>
+                ) : null}
+              </Focusable>
+            </Field>
+          </PanelSectionRow>
         </PanelSection>
         <PanelSection title="Shortcut Maker">
           <PanelSectionRow>
@@ -334,35 +466,98 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
               </DialogButton>
             </Field>
           </PanelSectionRow>
-          {/* TODO - jak 2 support */}
-          {/* <PanelSectionRow>
-        <Field childrenLayout="below" description="Adds a shortcut to your Steam Library for Jak 2">
-          <DialogButton
-            onClick={async () => {
-              const appId = (await serverAPI.callPluginMethod("create_shortcut", {
-                owner_id: getCurrentSteamUserId(),
-                game: "jak2"
-              })).result;
-              window.console.log(appId);
-              if (appId !== null || appId !== undefined) {
-                // Set small capsule
-                const smallImageData = (await serverAPI.callPluginMethod('read_small_image_as_base64', { game: "jak2" })).result;
-                if (smallImageData !== null && smallImageData !== undefined) {
-                  await SteamClient.Apps.SetCustomArtworkForApp(appId, smallImageData, 'png', 0);
-                }
-                // Set wide capsule
-                const largeImageData = (await serverAPI.callPluginMethod('read_large_image_as_base64', { game: "jak2" })).result;
-                if (largeImageData !== null && largeImageData !== undefined) {
-                  await SteamClient.Apps.SetCustomArtworkForApp(appId, largeImageData, 'png', 3);
-                }
-                showRestartConfirm();
+          <PanelSectionRow>
+            <Field
+              childrenLayout="below"
+              description={
+                jak2ShortcutAlreadyExists
+                  ? "Shortcut Already Exists"
+                  : "Adds a shortcut to your Steam Library for Jak 2"
               }
-            }}
-          >
-            Create Jak 2 Shortcut
-          </DialogButton>
-        </Field>
-      </PanelSectionRow> */}
+            >
+              <DialogButton
+                disabled={jak2ShortcutAlreadyExists}
+                onClick={async () => {
+                  const appId = (
+                    await serverAPI.callPluginMethod("create_shortcut", {
+                      owner_id: getCurrentSteamUserId(),
+                      game: "jak2",
+                    })
+                  ).result;
+                  window.console.log(appId);
+                  if (appId !== null && appId !== undefined) {
+                    // Set small capsule
+                    const smallImageData = (
+                      await serverAPI.callPluginMethod(
+                        "read_small_image_as_base64",
+                        { game: "jak2" }
+                      )
+                    ).result;
+                    if (
+                      smallImageData !== null &&
+                      smallImageData !== undefined
+                    ) {
+                      await SteamClient.Apps.SetCustomArtworkForApp(
+                        appId,
+                        smallImageData,
+                        "png",
+                        0
+                      );
+                    }
+                    // Set wide capsule
+                    const wideImageData = (
+                      await serverAPI.callPluginMethod(
+                        "read_wide_image_as_base64",
+                        { game: "jak2" }
+                      )
+                    ).result;
+                    if (wideImageData !== null && wideImageData !== undefined) {
+                      await SteamClient.Apps.SetCustomArtworkForApp(
+                        appId,
+                        wideImageData,
+                        "png",
+                        3
+                      );
+                    }
+                    // Set hero
+                    const heroImageData = (
+                      await serverAPI.callPluginMethod(
+                        "read_hero_image_as_base64",
+                        { game: "jak2" }
+                      )
+                    ).result;
+                    if (heroImageData !== null && heroImageData !== undefined) {
+                      await SteamClient.Apps.SetCustomArtworkForApp(
+                        appId,
+                        heroImageData,
+                        "png",
+                        1
+                      );
+                    }
+                    // Set logo
+                    const logoImageData = (
+                      await serverAPI.callPluginMethod(
+                        "read_logo_image_as_base64",
+                        { game: "jak2" }
+                      )
+                    ).result;
+                    if (logoImageData !== null && logoImageData !== undefined) {
+                      await SteamClient.Apps.SetCustomArtworkForApp(
+                        appId,
+                        logoImageData,
+                        "png",
+                        2
+                      );
+                    }
+                    // NOTE - icon is set in the shortcut!
+                    showRestartConfirm();
+                  }
+                }}
+              >
+                Create Jak 2 Shortcut
+              </DialogButton>
+            </Field>
+          </PanelSectionRow>
         </PanelSection>
         <PanelSection title="Links">
           <PanelSectionRow>
@@ -512,9 +707,14 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
   };
 
   const loadingComponent = () => {
-    return <PanelSection title="Loading...">
-      <p>If it remains loading for a while, this is a bug I've yet to figure out. You may have to restart your deck / re-open the plugin.</p>
-    </PanelSection>;
+    return (
+      <PanelSection title="Loading...">
+        <p>
+          If it remains loading for a while, this is a bug I've yet to figure
+          out. You may have to restart your deck / re-open the plugin.
+        </p>
+      </PanelSection>
+    );
   };
 
   return componentLoaded ? loadedComponent() : loadingComponent();
